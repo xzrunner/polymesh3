70b853d23379245819e4903a4bacb4df530742b1
zz <zhuguang@ejoy.com>
2019-08-02 20:36:39 +0800

    [ADDED] BrushVertex and BrushEdge

--- include/polymesh3/typedef.h
+++ include/polymesh3/typedef.h
@@ -5,6 +5,12 @@
 namespace pm3
 {
 
+typedef size_t                    BrushVertex;
+typedef std::pair<size_t, size_t> BrushEdge;
+
+typedef std::shared_ptr<BrushVertex> BrushVertexPtr;
+typedef std::shared_ptr<BrushEdge>   BrushEdgePtr;
+
 struct BrushFace;
 using BrushFacePtr = std::shared_ptr<BrushFace>;
 
